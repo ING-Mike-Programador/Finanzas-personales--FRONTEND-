d3dabdcd5488288e70649d3129d5a07b4fdbb89f
ING-Mike-Programador <miguel_alonso44@hotmail.com>
2026-08-13 17:42:40 -0600

    Archivos innecesarios eliminados

--- Finanzas_personales/src/app/app.config.ts
+++ Finanzas_personales/src/app/app.config.ts
@@ -3,11 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    // HAshstrategy
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
   ]
+
 };
