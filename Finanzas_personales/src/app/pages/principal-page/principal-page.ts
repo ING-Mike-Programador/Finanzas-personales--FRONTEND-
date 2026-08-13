31aee2f9b7c582105241d7fd3ce484c563693b17
ING-Mike-Programador <miguel_alonso44@hotmail.com>
2026-08-13 17:58:29 -0600

    Comienzo del proyecto, se agrego un modal como prueba de funcionalidad

---
+++ Finanzas_personales/src/app/pages/principal-page/principal-page.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-principal-pages',
+  imports: [],
+  templateUrl: './principal-page.html',
+})
+export default class PrincipalPage {}
