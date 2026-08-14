1962e042c1b06fe172b986503a50ada77e172fd2
ING-Mike-Programador <miguel_alonso44@hotmail.com>
2026-08-13 19:05:14 -0600

    Eliminacion de proyecto para comenzar de 0

--- Finanzas_personales/src/app/pages/principal-page/principal-page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'app-principal-pages',
-  imports: [],
-  templateUrl: './principal-page.html',
-})
-export default class PrincipalPage {}
