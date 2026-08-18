27031758764981c4e956f5a9cae3dfd46ed20c5b
ING-Mike-Programador <miguel_alonso44@hotmail.com>
2026-08-18 13:02:28 -0600

    Agregar modal para fuentes monetarias y mejorar estilos en la página principal

---
+++ finanzasPersonales/src/app/components/modal-Agg-FuenteMonetario/modal-Agg-FuenteMonetario.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'modal-agg-fuente-monetario',
+  imports: [],
+  templateUrl: './modal-Agg-FuenteMonetario.html',
+})
+export class ModalAggFuenteMonetario {}
--- finanzasPersonales/src/app/pages/page-Principal/page-Principal.ts
+++ finanzasPersonales/src/app/pages/page-Principal/page-Principal.ts
@@ -1,12 +1,32 @@
 import { Component, signal } from '@angular/core';
 import { TablaInfoMensual } from '../../components/tablaInfoMensual/tablaInfoMensual';
 import { ModalAggDataMovimiento } from '../../components/modal-Agg-DataMovimiento/modal-Agg-DataMovimiento';
+import { ModalAggFuenteMonetario } from "../../components/modal-Agg-FuenteMonetario/modal-Agg-FuenteMonetario";
 
 @Component({
   selector: 'app-page-principal',
-  imports: [TablaInfoMensual, ModalAggDataMovimiento],
+  imports: [TablaInfoMensual, ModalAggDataMovimiento, ModalAggFuenteMonetario],
   templateUrl: './page-Principal.html',
 })
 export default class PagePrincipal {
+  dayActual = signal(new Date()); // obtener la fecha actual
+  month = signal(new Date().getMonth()); // obtener el mes actual (los meses van de 0 a 11)
+  year = signal(new Date().getFullYear()); // obtener el año actual
+  daysInMonth = signal(new Date(this.year(), this.month() + 1, 0).getDate()); // obtener el número de días en el mes actual
 
+  // Array con los nombres de los meses
+  months = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+  ];
 }
